a0bc4f966cc0745da99515b9ff7bb8e2a1fc155c
yanding.li <David.Jackson.Lyd@gmail.com>
2022-09-20 22:56:26 +0800

    Update assets.ts

--- src/core/global-api/assets.ts
+++ src/core/global-api/assets.ts
@@ -1,3 +1,13 @@
+/*
+ * @Author: yanding.li David.Jackson.Lyd@gmail.com
+ * @Date: 2022-09-20 21:42:12
+ * @LastEditors: yanding.li David.Jackson.Lyd@gmail.com
+ * @LastEditTime: 2022-09-20 22:56:15
+ * @FilePath: \vue2-core\src\core\global-api\assets.ts
+ * @Description:
+ *
+ * Copyright (c) 2022 by yanding.li David.Jackson.Lyd@gmail.com, All Rights Reserved.
+ */
 import { ASSET_TYPES } from 'shared/constants'
 import type { GlobalAPI } from 'types/global-api'
 import { isFunction, isPlainObject, validateComponentName } from '../util/index'
@@ -6,6 +16,8 @@ export function initAssetRegisters(Vue: GlobalAPI) {
   /**
    * Create asset registration methods.
    */
+  // 遍历 ASSET_TYPES 数组，为 Vue 定义相应方法
+  // ASSET_TYPES 包括了directive、 component、filter
   ASSET_TYPES.forEach(type => {
     // @ts-expect-error function is not exact same type
     Vue[type] = function (
@@ -22,11 +34,14 @@ export function initAssetRegisters(Vue: GlobalAPI) {
         if (type === 'component' && isPlainObject(definition)) {
           // @ts-expect-error
           definition.name = definition.name || id
+          // 把组件配置转换为组件的构造函数
           definition = this.options._base.extend(definition)
         }
         if (type === 'directive' && isFunction(definition)) {
           definition = { bind: definition, update: definition }
         }
+        // 全局注册，存储资源并赋值
+        // this.options['components']['comp'] = definition
         this.options[type + 's'][id] = definition
         return definition
       }
