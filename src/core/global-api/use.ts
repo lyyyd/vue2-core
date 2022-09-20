1b26748a7790f7915e69fca85f594dda15c33d87
yanding.li <David.Jackson.Lyd@gmail.com>
2022-09-20 22:55:05 +0800

    Update use.ts

--- src/core/global-api/use.ts
+++ src/core/global-api/use.ts
@@ -1,3 +1,13 @@
+/*
+ * @Author: yanding.li David.Jackson.Lyd@gmail.com
+ * @Date: 2022-09-20 21:42:12
+ * @LastEditors: yanding.li David.Jackson.Lyd@gmail.com
+ * @LastEditTime: 2022-09-20 22:51:38
+ * @FilePath: \vue2-core\src\core\global-api\use.ts
+ * @Description:
+ *
+ * Copyright (c) 2022 by yanding.li David.Jackson.Lyd@gmail.com, All Rights Reserved.
+ */
 import type { GlobalAPI } from 'types/global-api'
 import { toArray, isFunction } from '../util/index'
 
@@ -10,7 +20,9 @@ export function initUse(Vue: GlobalAPI) {
     }
 
     // additional parameters
+    // 把数组中的第一个元素(plugin)去除
     const args = toArray(arguments, 1)
+    // 把this(Vue)插入第一个元素的位置
     args.unshift(this)
     if (isFunction(plugin.install)) {
       plugin.install.apply(plugin, args)
