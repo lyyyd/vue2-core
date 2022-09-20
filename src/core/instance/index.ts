4efb3acee32495f4b766c4622be3aa802ccfd744
yanding.li <David.Jackson.Lyd@gmail.com>
2022-09-20 22:35:41 +0800

    Update index.ts
    \vue2-core\src\core\instance\index.ts

--- src/core/instance/index.ts
+++ src/core/instance/index.ts
@@ -6,22 +6,36 @@ import { lifecycleMixin } from './lifecycle'
 import { warn } from '../util/index'
 import type { GlobalAPI } from 'types/global-api'
 
+// 此处不用 class 的原因是因为方便后续给 Vue 实例混入实例成员
 function Vue(options) {
   if (__DEV__ && !(this instanceof Vue)) {
     warn('Vue is a constructor and should be called with the `new` keyword')
   }
+  // 调用 _init() 方法
   this._init(options)
 }
 
 //@ts-expect-error Vue has function type
+// 注册 vm 的 _init() 方法，初始化 vm
 initMixin(Vue)
+
 //@ts-expect-error Vue has function type
+// 注册 vm 的 $data/$props/$set/$delete/$watch
 stateMixin(Vue)
+
 //@ts-expect-error Vue has function type
+// 初始化事件相关方法
+// $on/$once/$off/$emit
 eventsMixin(Vue)
+
 //@ts-expect-error Vue has function type
+// 初始化生命周期相关的混入方法
+// _update/$forceUpdate/$destroy
 lifecycleMixin(Vue)
+
 //@ts-expect-error Vue has function type
+// 混入 render
+// $nextTick/_render
 renderMixin(Vue)
 
 export default Vue as unknown as GlobalAPI
