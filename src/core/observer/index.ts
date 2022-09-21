d276a545beef92d08baec4f344bb5321477a1c5b
yanding.li <David.Jackson.Lyd@gmail.com>
2022-09-21 22:12:51 +0800

    Update index.ts

--- src/core/observer/index.ts
+++ src/core/observer/index.ts
@@ -47,14 +47,19 @@ const mockDep = {
  * collect dependencies and dispatch updates.
  */
 export class Observer {
+  // 依赖对象
   dep: Dep
+  // 实例计数器
   vmCount: number // number of vms that have this object as root $data
 
   constructor(public value: any, public shallow = false, public mock = false) {
     // this.value = value
     this.dep = mock ? mockDep : new Dep()
+    // 初始化实例的 vmCount 为0
     this.vmCount = 0
+    // 将实例挂载到观察对象的 __ob__ 属性
     def(value, '__ob__', this)
+    // 数组的响应式处理
     if (isArray(value)) {
       if (!mock) {
         if (hasProto) {
@@ -69,6 +74,7 @@ export class Observer {
         }
       }
       if (!shallow) {
+        // 为数组中的每一个对象创建一个 observer 实例
         this.observeArray(value)
       }
     } else {
@@ -77,6 +83,7 @@ export class Observer {
        * getter/setters. This method should only be called when
        * value type is Object.
        */
+      // 遍历对象中的每一个属性，转换成 setter/getter
       const keys = Object.keys(value)
       for (let i = 0; i < keys.length; i++) {
         const key = keys[i]
@@ -111,6 +118,7 @@ export function observe(
     return
   }
   let ob: Observer | void
+  // 如果 value 有 __ob__(observer对象) 属性 结束
   if (hasOwn(value, '__ob__') && value.__ob__ instanceof Observer) {
     ob = value.__ob__
   } else if (
@@ -120,11 +128,13 @@ export function observe(
     Object.isExtensible(value) &&
     !value.__v_skip /* ReactiveFlags.SKIP */
   ) {
+    // 创建一个 Observer 对象
     ob = new Observer(value, shallow, ssrMockReactivity)
   }
   return ob
 }
 
+// 为一个对象定义一个响应式的属性
 /**
  * Define a reactive property on an Object.
  */
@@ -136,13 +146,16 @@ export function defineReactive(
   shallow?: boolean,
   mock?: boolean
 ) {
+  // 创建依赖对象实例
   const dep = new Dep()
 
+  // 获取 obj 的属性描述符对象
   const property = Object.getOwnPropertyDescriptor(obj, key)
   if (property && property.configurable === false) {
     return
   }
 
+  // 提供预定义的存取器函数
   // cater for pre-defined getter/setters
   const getter = property && property.get
   const setter = property && property.set
@@ -153,12 +166,16 @@ export function defineReactive(
     val = obj[key]
   }
 
+  // 判断是否递归观察子对象，并将子对象属性都转换成 getter/setter，返回子观察对象
   let childOb = !shallow && observe(val, false, mock)
   Object.defineProperty(obj, key, {
     enumerable: true,
     configurable: true,
     get: function reactiveGetter() {
+      // 如果预定义的 getter 存在则 value 等于getter 调用的返回值
+      // 否则直接赋予属性值
       const value = getter ? getter.call(obj) : val
+      // 如果存在当前依赖目标，即 watcher 对象，则建立依赖
       if (Dep.target) {
         if (__DEV__) {
           dep.depend({
@@ -169,26 +186,34 @@ export function defineReactive(
         } else {
           dep.depend()
         }
+        // 如果子观察目标存在，建立子对象的依赖关系
         if (childOb) {
+          // 如果属性是数组，则特殊处理收集数组对象依赖
           childOb.dep.depend()
           if (isArray(value)) {
             dependArray(value)
           }
         }
       }
+      // 返回属性值
       return isRef(value) && !shallow ? value.value : value
     },
     set: function reactiveSetter(newVal) {
+      // 如果预定义的 getter 存在则 value 等于getter 调用的返回值
+      // 否则直接赋予属性值
       const value = getter ? getter.call(obj) : val
+      // 如果新值等于旧值或者新值旧值为NaN则不执行
       if (!hasChanged(value, newVal)) {
         return
       }
       if (__DEV__ && customSetter) {
         customSetter()
       }
+      // 如果没有 setter 直接返回
       if (setter) {
         setter.call(obj, newVal)
       } else if (getter) {
+        // 如果预定义setter存在则调用，否则直接更新新值
         // #7981: for accessor properties without setter
         return
       } else if (!shallow && isRef(value) && !isRef(newVal)) {
@@ -197,6 +222,7 @@ export function defineReactive(
       } else {
         val = newVal
       }
+      // 如果新值是对象，观察子对象并返回 子的 observer 对象
       childOb = !shallow && observe(newVal, false, mock)
       if (__DEV__) {
         dep.notify({
@@ -207,6 +233,7 @@ export function defineReactive(
           oldValue: value
         })
       } else {
+        // 派发更新(发布更改通知)
         dep.notify()
       }
     }
@@ -237,8 +264,11 @@ export function set(
     return
   }
   const ob = (target as any).__ob__
+  // 判断 target 是否是对象，key 是否是合法的索引
   if (isArray(target) && isValidArrayIndex(key)) {
     target.length = Math.max(target.length, key)
+    // 通过 splice 对key位置的元素进行替换
+    // splice 在 array.js 进行了响应化的处理
     target.splice(key, 1, val)
     // when mocking for SSR, array methods are not hijacked
     if (ob && !ob.shallow && ob.mock) {
@@ -246,10 +276,12 @@ export function set(
     }
     return val
   }
+  // 如果 key 在对象中已经存在直接赋值
   if (key in target && !(key in Object.prototype)) {
     target[key] = val
     return val
   }
+  // 如果 target 是 vue 实例或者 $data 直接返回
   if ((target as any)._isVue || (ob && ob.vmCount)) {
     __DEV__ &&
       warn(
@@ -258,10 +290,12 @@ export function set(
       )
     return val
   }
+  // 如果 ob 不存在，target 不是响应式对象直接赋值
   if (!ob) {
     target[key] = val
     return val
   }
+  // 把 key 设置为响应式属性
   defineReactive(ob.value, key, val, undefined, ob.shallow, ob.mock)
   if (__DEV__) {
     ob.dep.notify({
@@ -272,6 +306,7 @@ export function set(
       oldValue: undefined
     })
   } else {
+    // 发送通知
     ob.dep.notify()
   }
   return val
@@ -288,11 +323,16 @@ export function del(target: any[] | object, key: any) {
       `Cannot delete reactive property on undefined, null, or primitive value: ${target}`
     )
   }
+  // 判断是否是数组，以及 key 是否合法
   if (isArray(target) && isValidArrayIndex(key)) {
+    // 如果是数组通过 splice 删除
+    // splice 做过响应式处理
     target.splice(key, 1)
     return
   }
+  // 获取 target 的 ob 对象
   const ob = (target as any).__ob__
+  // target 如果是 Vue 实例或者 $data 对象，直接返回
   if ((target as any)._isVue || (ob && ob.vmCount)) {
     __DEV__ &&
       warn(
@@ -306,13 +346,16 @@ export function del(target: any[] | object, key: any) {
       warn(`Delete operation on key "${key}" failed: target is readonly.`)
     return
   }
+  // 如果 target 对象没有 key 属性直接返回
   if (!hasOwn(target, key)) {
     return
   }
+  // 删除属性
   delete target[key]
   if (!ob) {
     return
   }
+  // 通过 ob 发送通知
   if (__DEV__) {
     ob.dep.notify({
       type: TriggerOpTypes.DELETE,
@@ -320,6 +363,7 @@ export function del(target: any[] | object, key: any) {
       key
     })
   } else {
+    // 通过 ob 发送通知
     ob.dep.notify()
   }
 }
