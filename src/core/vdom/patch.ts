5f6805454531d2d5391db70de01a3beeef573caf
yanding.li <David.Jackson.Lyd@gmail.com>
2022-09-23 21:33:17 +0800

    Update patch.ts

--- src/core/vdom/patch.ts
+++ src/core/vdom/patch.ts
@@ -200,7 +200,9 @@ export function createPatchFunction(backend) {
       // component also has set the placeholder vnode's elm.
       // in that case we can just return the element and be done.
       if (isDef(vnode.componentInstance)) {
+        // 调用钩子函数（VNode的钩子函数初始化属性/事件/样式等，组件的钩子函数）
         initComponent(vnode, insertedVnodeQueue)
+        // 把组件对应的 DOM 插入到父元素中
         insert(parentElm, vnode.elm, refElm)
         if (isTrue(isReactivated)) {
           reactivateComponent(vnode, insertedVnodeQueue, parentElm, refElm)
