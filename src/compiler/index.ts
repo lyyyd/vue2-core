4695c79b2db75872e89107e4342e949d2cea5c33
yanding.li <David.Jackson.Lyd@gmail.com>
2022-09-24 06:27:56 +0800

    Update index.ts

--- src/compiler/index.ts
+++ src/compiler/index.ts
@@ -1,3 +1,13 @@
+/*
+ * @Author: yanding.li David.Jackson.Lyd@gmail.com
+ * @Date: 2022-09-20 21:42:12
+ * @LastEditors: yanding.li David.Jackson.Lyd@gmail.com
+ * @LastEditTime: 2022-09-24 06:27:52
+ * @FilePath: \vue2-core\src\compiler\index.ts
+ * @Description:
+ *
+ * Copyright (c) 2022 by yanding.li David.Jackson.Lyd@gmail.com, All Rights Reserved.
+ */
 import { parse } from './parser/index'
 import { optimize } from './optimizer'
 import { generate } from './codegen/index'
@@ -11,14 +21,20 @@ export const createCompiler = createCompilerCreator(function baseCompile(
   template: string,
   options: CompilerOptions
 ): CompiledResult {
+  // 把模板转换成 ast 抽象语法树
+  // 抽象语法树，用来以树形的方式描述代码结构
   const ast = parse(template.trim(), options)
   if (options.optimize !== false) {
+    // 优化抽象语法树
     optimize(ast, options)
   }
+  // 把抽象语法树生成字符串形式的 js 代码
   const code = generate(ast, options)
   return {
     ast,
+    // 渲染函数
     render: code.render,
+    // 静态渲染函数，生成静态 VNode 树
     staticRenderFns: code.staticRenderFns
   }
 })
